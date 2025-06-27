1e23b43bfd7d036ae934df0a088ff40277788888
Aaron Hill <133811546+aaronfpp@users.noreply.github.com>
2025-06-27 11:48:56 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ function toggleMenu() {
     const menu = document.getElementById('nav-menu');
     menu.classList.toggle('open');
 }
+
+function toggleChat() {
+    const chatPopup = document.getElementById('chat-popup');
+    chatPopup.classList.toggle('open');
+}
